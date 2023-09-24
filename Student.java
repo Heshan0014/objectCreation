f853337edc5608f1d25ef62200107c2a38c4fa97
MHMH Hiruwinda <140095565+Heshan0014@users.noreply.github.com>
2023-09-24 20:03:59 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,17 @@
+
+
+
+class Student{
+
+    String x = "Sam";
+    String y = "John";
+
+    public static void main(String[] args) {
+        Student Obj1 = new Student();
+        Student Obj2 = new Student();
+
+
+        System.out.println(Obj1.x);
+        System.out.println(Obj2.y);
+}
+}
